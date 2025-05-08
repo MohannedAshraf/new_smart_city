bce2fad589a9383ca9a55562b465601d3c3b330d
mohanned <MohannedAshraf GitHub>
2025-05-08 21:42:31 +0300

    add  reset pass and  otp pages

---
+++ lib/screens/otp_page.dart
@@ -0,0 +1,108 @@
+// ignore_for_file: library_private_types_in_public_api, prefer_final_fields
+
+import 'package:citio/screens/mylogin_page.dart';
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class VerificationScreen extends StatefulWidget {
+  const VerificationScreen({super.key});
+
+  @override
+  _VerificationScreenState createState() => _VerificationScreenState();
+}
+
+class _VerificationScreenState extends State<VerificationScreen> {
+  TextEditingController _pinController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: const Text('رمز التحقق')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            const Center(
+              child: Text(
+                'أدخل رمز التحقق المكون من 4 أرقام',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            SizedBox(height: 20),
+            PinCodeTextField(
+              appContext: context,
+              length: 4,
+              controller: _pinController,
+              keyboardType: TextInputType.number,
+              animationType: AnimationType.fade,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(10),
+                fieldHeight: 50,
+                fieldWidth: 40,
+                activeFillColor: Colors.white,
+                selectedFillColor: Colors.white,
+                inactiveFillColor: Colors.white,
+                activeColor: Colors.blue,
+                selectedColor: Colors.orange,
+                inactiveColor: Colors.grey,
+              ),
+              enableActiveFill: true,
+              onChanged: (value) {
+                // ignore: avoid_print
+                print("Current PIN: $value");
+              },
+              onCompleted: (code) {
+                // ignore: avoid_print
+                print("Completed: $code");
+                // هنا تحقق من الكود أو أرسله للسيرفر
+              },
+            ),
+
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "اعادة ارسال الرمز",
+                style: TextStyle(
+                  fontSize: 14,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: double.infinity,
+              height: 50,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MyloginPage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  "ارسال",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
