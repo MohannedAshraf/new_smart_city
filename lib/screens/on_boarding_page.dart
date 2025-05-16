0dd448f3f9dadc53c47fe6394dd7aca021533e8e
mohanned <MohannedAshraf GitHub>
2025-05-17 02:34:52 +0300

    edit splash screen

--- lib/screens/on_boarding_page.dart
+++ lib/screens/on_boarding_page.dart
@@ -16,7 +16,7 @@ class _StartPageState extends State<StartPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const MyloginPage()),
