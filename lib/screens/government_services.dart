f10ee633e934bab6a254b121d12b1e33f92fb1d4
mohanned <MohannedAshraf GitHub>
2025-06-22 13:40:48 +0300

    add const

--- lib/screens/government_services.dart
+++ lib/screens/government_services.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use, prefer_const_constructors, library_private_types_in_public_api
+
 import 'package:citio/core/utils/mycolors.dart';
 import 'package:citio/core/widgets/search_bar.dart';
 import 'package:citio/screens/government_service_details.dart';
