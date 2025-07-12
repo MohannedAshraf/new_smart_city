73ee8aea5f80d58016295610bab1298a9792daab
Saad Elsayed Saad <saadelsayed721@gmail.com>
2025-07-12 18:03:18 +0300

    reaction changed

--- lib/core/widgets/reaction_icon_mapper.dart
+++ lib/core/widgets/reaction_icon_mapper.dart
@@ -1,90 +1,58 @@
-// ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
 class ReactionIconMapper {
   static Widget getReactionIcon(String? type, {double size = 24}) {
- 
     return Builder(
       builder: (context) {
         final screenWidth = MediaQuery.of(context).size.width;
-        final scaledSize = screenWidth * 0.06; // يعادل تقريبًا 24
-
+        final scaledSize = screenWidth * 0.06;
         final iconSize = size != 24 ? size : scaledSize;
 
-        switch (type) {
-          case 'like':
-            return _buildIcon(
-              icon: Symbols.thumb_up,
-              color: Colors.blue,
-              size: iconSize,
-              shadowColor: Colors.blueAccent,
-            );
-          case 'love':
-            return _buildIcon(
-              icon: Symbols.favorite,
-              color: Colors.red,
-              size: iconSize,
-              shadowColor: Colors.redAccent,
-            );
-          case 'care':
-            return _buildIcon(
-              icon: Symbols.volunteer_activism,
-              color: Colors.orange,
-              size: iconSize,
-              shadowColor: Colors.orangeAccent,
-            );
-          case 'laugh':
-            return _buildIcon(
-              icon: Symbols.sentiment_very_satisfied,
-              color: Colors.yellow.shade700,
-              size: iconSize,
-              shadowColor: Colors.yellow.shade300,
-            );
-          case 'sad':
-            return _buildIcon(
-              icon: Symbols.sentiment_dissatisfied,
-              color: Colors.blueGrey,
-              size: iconSize,
-              shadowColor: Colors.blueGrey.shade200,
-            );
-          case 'hate':
-            return _buildIcon(
-              icon: Symbols.heart_broken,
-              color: const Color.fromARGB(255, 1, 0, 3),
-              size: iconSize,
-              shadowColor: Colors.deepPurpleAccent,
-            );
-          default:
-            return Icon(
-              Symbols.emoji_emotions,
-              color: Colors.grey.shade500,
-              size: iconSize,
-            );
+        final isSupported = _isSupportedType(type);
+
+        if (isSupported) {
+          final imagePath = 'assets/reactions/$type.png';
+          return Container(
+            width: iconSize,
+            height: iconSize,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.08),
+                  blurRadius: 3,
+                  offset: const Offset(0, 1.5),
+                ),
+              ],
+            ),
+            child: ClipOval(
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.contain,
+                errorBuilder: (context, error, stackTrace) {
+                  return _defaultIcon(iconSize);
+                },
+              ),
+            ),
+          );
+        } else {
+          return _defaultIcon(iconSize);
         }
       },
     );
   }
 
-  static Widget _buildIcon({
-    required IconData icon,
-    required Color color,
-    required double size,
-    required Color shadowColor,
-  }) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: shadowColor.withOpacity(0.1),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
-        ],
-        shape: BoxShape.circle,
-      ),
-      child: Icon(icon, color: color, size: size),
+  static bool _isSupportedType(String? type) {
+    const supported = ['like', 'love', 'care', 'laugh', 'sad', 'hate'];
+    return supported.contains(type);
+  }
+
+  static Widget _defaultIcon(double size) {
+    return Icon(
+      Symbols.emoji_emotions,
+      color: Colors.grey.shade500,
+      size: size,
     );
   }
 }
